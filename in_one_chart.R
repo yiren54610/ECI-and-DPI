c27ba142f18d30c2ecbe8be8d7bc333fb0533e8d
yiren54610 <yl5732@columbia.edu>
2025-02-05 12:54:19 -0500

    draft2

--- in_one_chart.R
+++ in_one_chart.R
@@ -1,6 +1,7 @@
 # in one chart plot 
 # install a package
 install.packages("tidyverse")
+require("tidyverse")
 
 # import 
 require("readr")
@@ -11,11 +12,7 @@ setwd("~/Documents/r-studio/in_one_chart")
 
 ECI = read.csv('DSPI(U.S. Bureau of Economic Analysi) - DSPI.csv')
 
-remove.packages("ggplot2") # Unisntall ggplot
-install.packages("ggplot2") # Install it again
-require(ggplot2)
-
-# ECI plot
+# explore: ECI dot plot
 ggplot(ECI) +
   aes(x = observation_date, y = ECI, color = ifelse(ECI > 0, "green", "red")) +
   geom_point() +
@@ -24,25 +21,27 @@ ggplot(ECI) +
   labs(title = 'Economic Confidence Index (trend)', 
        subtitle = "ECI change from Trump's first administration to Biden's administration")
 
+# explore: ECI vs DSPI
 ggplot(ECI) +
   aes(x=DSPI, y=ECI) +
   geom_point()
 
 # convert char to date
 ECI$date = as.Date(ECI$observation_date)
-  
+
+# line chart ECI  
 ggplot(ECI) +
   aes(x=date,y=ECI,group=1)+
   geom_line() +
   scale_x_date(date_breaks = "1 year",date_labels = "%Y")
 
-# combined chart
+# DCPI Line chart
 ggplot(ECI) +
   aes(x=date,y=DSPI,group=1)+
   geom_line() +
   scale_x_date(date_breaks = "1 year",date_labels = "%Y")
 
-# DSPI plot
+
 # 1. Reshape the data to long format
 library(dplyr)
 df <- ECI %>%
@@ -56,18 +55,8 @@ df_long <- df %>%
                names_to = "Category", 
                values_to = "Amount")
 
-# Create the combined chart
-ggplot(df_long, aes(x = date, y = Amount, fill = Category)) +
-  geom_bar(stat = "identity",alpha = 0.5) +  
-  labs(title = "Post-COVID Reality: Income Rises But Pessimism Lingers",
-       subtitle = "Comparing Real Disposable Personal Income (DSPI) to the Economic Confidence Index During Trump's Final Administration and Biden's Administration",
-       x = "Observation Date",
-       y = "Amount") +
-  facet_wrap(~Category,ncol=1,scales='free_y') +
-  theme_minimal() +
-  scale_x_date(date_breaks = "1 year",date_labels = "%Y")
 
-#adMINISTRATION
+#ADMINISTRATION
 presidents <- data.frame(
   xmin = as.Date(c("2017-01-20", "2021-01-20")),
   xmax = as.Date(c("2021-01-19", "2024-12-31")),
@@ -76,43 +65,59 @@ presidents <- data.frame(
   president = factor(c("Trump", "Biden")) # This will be used for the legend
 )
 
-# another type
-chart <- ggplot(df_long,aes(x = date, y = Amount)) +
-  geom_rect(data = presidents, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = president), 
-            alpha = 0.2, inherit.aes = FALSE) +
-  geom_bar(data = subset(df_long, Category != "ECI"), 
-           stat = "identity", 
-           alpha = 0.5,
-           show.legend = FALSE) +  
+# change order of category
+df_long$Category <- factor(df_long$Category, 
+                           levels = c("ECI","DSPI"), 
+                           labels = c("People's confidence towards the economy tanked after covid.", 
+                                      "but real incomes were rising the whole time."))
+
+# Combined Graph
+chart2 <-ggplot(df_long,aes(x = date, y = Amount)) +
+  geom_rect(data = presidents, 
+            aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, 
+                fill = president), 
+            alpha = 0.2, 
+            inherit.aes = FALSE) +
+  # ECI first
   geom_line(data = subset(df_long, Category == "ECI"), 
             aes(color = Category), 
-            size = 1,
-            show.legend = FALSE) +
-  geom_hline(data = data.frame(yintercept = 0, Category = "ECI"),
+            linewidth = 1,
+            show.legend = FALSE,
+            na.rm = TRUE) +
+  # DSPI second
+  geom_line(data = subset(df_long, Category != "ECI"), 
+            aes(color = Category), 
+            linewidth = 1,
+           show.legend = FALSE,
+           na.rm = TRUE) +  
+  geom_hline(data = subset(df_long, Category == "People's confidence towards the economy tanked after covid."), 
+             aes(yintercept = 0), 
+             alpha = .6,
              linetype = "dotted",
-             yintercept = 0, 
              color = "red", 
              linewidth = .8,
-             alpha = ifelse(Category == "DSPI", 0, 0.6)) +
+             show.legend = FALSE) +
   labs(title = "Post-COVID Reality: Income Rises But Pessimism Lingers",
        subtitle = "Comparing Real Disposable Personal Income (DSPI) to the Economic 
        Confidence Index During Trump's Last Administration and Biden's Administration",
        fill = "Presidency",
        x = NULL,
        y = NULL) +
-  facet_wrap(~Category, ncol = 1, scales = 'free_y') +
+  facet_wrap(~Category, ncol = 1, 
+             scales = 'free_y',
+             as.table = TRUE) +
   theme_minimal() +
   scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
   scale_fill_manual(values = c("Trump" = "lightcoral", "Biden" = "lightblue")) +
   theme(legend.position = "bottom",
         plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),
         plot.subtitle = element_text(hjust = 0.5, size = 12)) +
-  annotate("text", x = 2020-03-01:2020-04-01, y = 25, 
-           label = "First lockdown on March 16th in US",
-           parse=TRUE)
+  geom_text(data = subset(df_long, Category == "People's confidence towards the economy tanked after covid."), 
+            aes(x = as.Date("2020-03-16"), y = 20), 
+           label = "First lockdown in US on March 16, 2020", 
+           color = "azure4", size = 2, hjust = 0, vjust = -1)
 
-chart
 
-ggsave("In_One_Chart.png")
+ggsave("In_One_Chart2.png")
 
 
